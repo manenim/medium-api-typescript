3c02209bb3cd463634abad8f77dd45f8b4883874
Manenimabasi Udoh (Dev Mani) <85757137+manenim@users.noreply.github.com>
2023-08-17 12:10:58 +0100

    Delete build directory

--- build/config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const mongoUri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gckpzfq.mongodb.net/?retryWrites=true&w=majority`;
-exports.default = { mongoUri };
